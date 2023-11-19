ab54668485d3a1dacf0ecf9e5e5c5c9753d37757
maclarenscott <maclarenscottdev@gmail.com>
2023-11-19 07:49:05 -0500

    cors

--- webapp/src/pages/api/submit.js
+++ webapp/src/pages/api/submit.js
@@ -3,6 +3,16 @@ import { makeOpenAIRequest } from "@/functions/parse_gpt";
 import { promisify } from 'util'; // Import promisify
 
 export default async function handler(req, res) {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this to be more restrictive if needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle preflight requests for CORS
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
     try {
         // Connect to the database
         console.log("Gonna connect to the database");
@@ -27,15 +37,15 @@ export default async function handler(req, res) {
         // Insert the form data into the database
         const q = `INSERT INTO submissions_prod (email, availability, name, skill) VALUES (?, ?, ?, ?)`;
         // const q = `INSERT INTO submissions (email, availability, name, skill) VALUES (?, ?, ?, ?)`;
-        
+
         // Promisify the query function
         const queryPromise = promisify(con.query).bind(con);
         await queryPromise(q, [email, parsed_availability, name, 'empty']);
 
         console.log("1 record inserted");
         //resolve the promise
-     
-        
+
+
 
         con.end();
     } catch (error) {
@@ -44,7 +54,7 @@ export default async function handler(req, res) {
         res.status(500).json({ status: 'error', message: 'Internal Server Error' });
     } finally {
         // Close the database connection
-        
+
         //resolve the promise
         res.status(200).json({ status: 'success', message: 'Form submitted successfully' });
     }
